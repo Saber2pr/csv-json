9a82a3c2f05940ec078e808a5388c7ae8bd3ba09
saber2pr <saber2pr@gmail.com>
2019-03-22 18:59:34 +0000

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * @Author: saber2pr 
  * @Date: 2019-03-22 11:55:31 
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-03-22 18:40:11
+ * @Last Modified time: 2019-03-22 18:57:43
  */
 var name
 /**
@@ -24,7 +24,7 @@ function onCsvChange(event) {
   var value = event.target.value
   var json = JSON.stringify(toJson(value), null, 2)
   document.getElementById('json').value = json
-  download('download', name, document.getElementById('csv').value)
+  updateDownload()
 }
 /**
  * onJsonChange
@@ -35,31 +35,40 @@ function onJsonChange(event) {
   var value = event.target.value
   var csv = toCsv(value)
   document.getElementById('csv').value = csv
-  download('download', name, document.getElementById('json').value)
+  updateDownload()
 }
 
 document.getElementById('input').addEventListener('change', function (event) {
   ReaderAsync(event.target['files'][0]).then(function (res) {
     if (isType(event, 'csv')) {
+      name = event.target['files'][0].name.replace('csv', 'json')
       document.getElementById('csv').value = res
       return toJson(res)
     } else if (isType(event, 'json')) {
+      name = event.target['files'][0].name.replace('json', 'csv')
       document.getElementById('json').value = res
       return toCsv(res)
     } else {
       alert('文件格式错误!')
     }
   }).then(function (res) {
-    var content
     if (isType(event, 'csv')) {
-      name = event.target['files'][0].name.replace('csv', 'json')
-      content = JSON.stringify(res, null, 2)
-      document.getElementById('json').value = content
+      document.getElementById('json').value = JSON.stringify(res, null, 2)
     } else if (isType(event, 'json')) {
-      name = event.target['files'][0].name.replace('json', 'csv')
-      content = res
       document.getElementById('csv').value = res
     }
-    download('download', name, content)
+    updateDownload()
   })
 })
+/**
+ * updateDownload
+ */
+function updateDownload() {
+  if (name.split('.')[1] === 'json') {
+    download('download', name, document.getElementById('json').value)
+  } else if (name.split('.')[1] === 'csv') {
+    download('download', name, document.getElementById('csv').value)
+  } else {
+    alert('文件格式错误!')
+  }
+}
